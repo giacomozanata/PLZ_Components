3b1e69e8cef6c2bae59c0c3be8591bb6e4e5259a
giacomozanata <giacomozanata98@gmail.com>
2017-06-27 10:17:35 +0200

    database popolato

--- db/Pezzi.sql
+++ db/Pezzi.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Creato il: Giu 09, 2017 alle 10:38
+-- Creato il: Giu 27, 2017 alle 10:16
 -- Versione del server: 10.1.10-MariaDB
 -- Versione PHP: 7.0.4
 
@@ -35,6 +35,14 @@ CREATE TABLE `acquisti` (
   `Quantita` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dump dei dati per la tabella `acquisti`
+--
+
+INSERT INTO `acquisti` (`ID_Acquisto`, `FK_P_Iva`, `FK_Cod_Articolo`, `Data_Acquisto`, `Prezzo`, `Quantita`) VALUES
+(1, '12345678901', '3476V', '2017-06-20', 14, 45),
+(2, '34567890123', '3652F', '2017-06-20', 4521, 2);
+
 -- --------------------------------------------------------
 
 --
@@ -52,7 +60,9 @@ CREATE TABLE `articoli` (
 --
 
 INSERT INTO `articoli` (`Cod_Articolo`, `Descrizione`, `Quantita`) VALUES
-('', 'SELEZIONA UN ARTICOLO O AGGIUNGINE UNO', 0);
+('', 'SELEZIONA UN ARTICOLO O AGGIUNGINE UNO', 0),
+('3476V', 'OLIO CASTROL SYNTEC 5W30', 45),
+('3652F', 'BLOCCO MOTORE MERCEDES CLASSE C', 1);
 
 -- --------------------------------------------------------
 
@@ -73,7 +83,13 @@ CREATE TABLE `cliente` (
 --
 
 INSERT INTO `cliente` (`Codice_Fiscale`, `Nome_O_Ragione_Sociale`, `Indirizzo`, `P_Iva`, `Telefono`) VALUES
-('', 'SELEZIONA UN CLIENTE O AGGIUNGINE UNO', '', '', '');
+('', 'SELEZIONA UN CLIENTE O AGGIUNGINE UNO', '', '', ''),
+('CPSLLR70P29H594P', 'Caio Sagese', 'Piazza della Repubblica, 37 95121-Zia Lisa CT', '65432109876', '3632878912'),
+('DNCHSP81R68H011R', 'Blaga Antunovic', 'Via San Cosmo fuori Porta Nolana, 134 20066-Melzo MI', '', '3465438975'),
+('LLDSMQ86M52A539I', 'Alice Palerma', 'Via delle Coste, 54 42010-Toano RE', '87654321098', '3890987654'),
+('RFCGYS50S41I310I', 'Alen Ortega Gracia', 'Via Santa Teresa degli Scalzi, 119 35040-Este PD', '', '3478136213'),
+('TMNRNR78C27L529O', 'Omero Buccho', 'Via Bologna, 62 26838-Tavazzano Con Villavesco LO', '10987654321', '3969964544'),
+('YKZFSM74T65E982W', 'Kang Lei', 'Via Longhena, 128 00030-Gavignano RM', '', '3665225987');
 
 -- --------------------------------------------------------
 
@@ -93,7 +109,11 @@ CREATE TABLE `fornitori` (
 --
 
 INSERT INTO `fornitori` (`P_Iva`, `Ragione_Sociale`, `Indirizzo`, `Telefono`) VALUES
-('', 'SELEZIONA UN FORNITORE O AGGIUNGINE UNO', '', '');
+('', 'SELEZIONA UN FORNITORE O AGGIUNGINE UNO', '', ''),
+('12345678901', 'Sofa Express', 'Via Santa Lucia, 17 06020-Colpalombo PG', '3439143235'),
+('2345678901', 'Exact Realty', 'Via Palermo, 40 20161-Affori MI', '3682332165'),
+('34567890123', 'Greenwich IGA', 'Via Piccinni, 136 82010-Ripabianca Tressanti BN', '3678305518'),
+('45678901234', 'Chase Pitkin', 'Via Santa Teresa, 5 53020-Monte Amiata SI', '3796163476');
 
 -- --------------------------------------------------------
 
@@ -109,6 +129,13 @@ CREATE TABLE `vendite` (
   `Prezzo` float NOT NULL,
   `Quantita` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dump dei dati per la tabella `vendite`
+--
+
+INSERT INTO `vendite` (`Id_Vendita`, `FK_Cod_Articolo`, `FK_Codice_Fiscale`, `Data`, `Prezzo`, `Quantita`) VALUES
+(1, '3652F', 'RFCGYS50S41I310I', '2017-06-20', 5300, 1);
 
 --
 -- Indici per le tabelle scaricate
@@ -156,12 +183,12 @@ ALTER TABLE `vendite`
 -- AUTO_INCREMENT per la tabella `acquisti`
 --
 ALTER TABLE `acquisti`
-  MODIFY `ID_Acquisto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `ID_Acquisto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT per la tabella `vendite`
 --
 ALTER TABLE `vendite`
-  MODIFY `Id_Vendita` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `Id_Vendita` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- Limiti per le tabelle scaricate
 --
